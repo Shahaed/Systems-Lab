de5a3f2868db294a59bd20fd278fda2f9a4f410d
Shahaed <Shahaed77@gmail.com>
2016-02-24 01:58:29 -0600

    Added files via upload

---
+++ Lab5.ino
@@ -0,0 +1,18 @@
+
+void setup()
+{
+
+	pinMode(13, OUTPUT);
+
+
+}
+
+void loop()
+{
+
+	digitalWrite(13, HIGH);   // turn the LED on 
+	delay(2000);              // 2 second delay
+	digitalWrite(13, LOW);    // turn the LED off by making the voltage 0
+     
+
+}
